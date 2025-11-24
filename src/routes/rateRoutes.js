bce6de1cec4662f7650dc0534cdf759324e29d53
goutamgurjar09 <goutamgurjar09@gmail.com>
2025-11-24 23:24:33 +0530

    complted mashine task

--- src/routes/rateRoutes.js
+++ src/routes/rateRoutes.js
@@ -3,5 +3,5 @@ const  rateControllers  = require("../controllers/rateControllers");
 const rateLimiter = require("../middleware/rateLimiter");
 const router = express.Router();
 
-router.get("/rate-history", rateLimiter, rateControllers.getRateHistory);
+router.get("/data", rateLimiter, rateControllers.getRateHistory);
 module.exports = router;
